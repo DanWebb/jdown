ea54b8ead84852da9080336bdef403cce4133062
Dan Webb <mail@danwebb.co>
2019-01-23 12:16:01 +0000

    Exclude assets and unwanted files from buffer transformation

--- src/transform-buffer-to-string.ts
+++ src/transform-buffer-to-string.ts
@@ -1,8 +1,13 @@
+import path from 'path';
 import {Plugin} from 'metalsmith';
 
 const transfomBufferToString = () => {
   const transform: Plugin = (files, _, done) => {
     Object.keys(files).forEach(file => {
+      if (file.includes('.DS_Store') || file.includes(`assets${path.sep}`)) {
+        return;
+      }
+
       files[file].contents = files[file].contents.toString('utf8');
     });
 
